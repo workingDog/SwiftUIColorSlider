4ad71541dab3c03351fa5e6a21f545ebd8c3099b
workingdog <ringowathelet@gmail.com>
2019-08-19 12:47:48 +0900

    Initial Commit

---
+++ colorslider/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  colorslider
+//
+//  Created by Ringo Wathelet on 2019/08/19.
+//  Copyright © 2019 Ringo Wathelet. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
