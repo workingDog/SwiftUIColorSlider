f626a709ce8a51e0102d83c56118ba170223b0d0
workingdog <ringowathelet@gmail.com>
2019-08-29 13:27:30 +0900

    minor update

--- colorslider/ContentView.swift
+++ colorslider/ContentView.swift
@@ -15,19 +15,16 @@ struct ContentView: View {
     
     @ObservedObject var colorObject1 = ColorObject()
     @ObservedObject var colorObject2 = ColorObject()
-    
-    @State var slider1: ColorSlider?
-    @State var slider2: ColorSlider?
       
     var body: some View {
         VStack (spacing: 20) {
             
-            slider1
+            ColorSlider(colorObject: colorObject1, width: 300, height: 40, cornerRadius: 20)
             Text("\(colorObject1.value)")
             Text(String(colorObject1.color.description)).foregroundColor(colorObject1.color == .white ? .black : colorObject1.color)
             Circle().frame(width: 50, height: 50).overlay(Circle().stroke(Color.black,lineWidth: 1)).foregroundColor(colorObject1.color)
             
-            slider2
+            ColorSlider(colorObject: colorObject2, width: 300, height: 40, cornerRadius: 20)
             Text("\(colorObject2.value)")
             Text(String(colorObject2.color.description)).foregroundColor(colorObject2.color == .white ? .black : colorObject2.color)
             Circle().frame(width: 50, height: 50).overlay(Circle().stroke(Color.black,lineWidth: 1)).foregroundColor(colorObject2.color)
@@ -35,14 +32,10 @@ struct ContentView: View {
         }.onAppear(perform: doSetup)
     }
      
-    func doSetup() {
-        // using the default ColorObject settings
-        slider1 = ColorSlider(colorObject: colorObject1, width: 300, height: 40, cornerRadius: 20)
-        
+    func doSetup() { 
         // example of using the blockSize to modify the thickness of the color band
         colorObject2.nColors = 10
         colorObject2.blockSize = 10
-        slider2 = ColorSlider(colorObject: colorObject2, width: 300, height: 40, cornerRadius: 20)
     }
 }
 
